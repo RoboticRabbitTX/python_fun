53f378681c8075125d7ef845575b11c74f3fdd10
RoboticRabbitTX <elliott.dente@gmail.com>
2022-11-08 11:13:21 -0600

    made it cleaner and easier to read

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 weight = float(input("Weight: "))
 lbs_or_kg = input("(K)g or (L)bs: ")
 if lbs_or_kg == "L" or "l":
-    othr_weight = weight / 2.2
+    othr_weight = "{:.1f}".format(weight / 2.2)
     final_text = "Weight in Kg: "
 elif lbs_or_kg == "K" or "k":
-    othr_weight = weight * 2.2
+    othr_weight = "{:.1f}".format(weight * 2.2)
     final_text = "Weight in lbs: "
-othr_weight_str = "{:.1f}".format(othr_weight) # How u change floats decimal place
-print(final_text + othr_weight_str)
+print(f"{final_text}{othr_weight}")
