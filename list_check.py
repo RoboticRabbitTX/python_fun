7810c8030948b69a1c8247fe3f4e711760913511
RoboticRabbitTX <elliott.dente@gmail.com>
2022-11-11 19:54:13 -0600

    accept a string from the user and display characters that are present at an even index number

---
+++ list_check.py
@@ -0,0 +1,10 @@
+# gives boolean if first and last number of list is equal
+list = [5, 8, 10, 15, 7, 6, 3, 5]
+last_idx = len(list) - 1
+
+print(list[0] == list[last_idx])
+
+# if list[0] == list[last_idx]:
+#     print("True")
+# else:
+#     print("False")
